5e2ca00cd1b97d8b9a86cb11484d9168616b648b
Brandon Eng <beng713@gmail.com>
2020-08-08 15:01:06 -0700

    Python Weather Site

---
+++ README.md
@@ -0,0 +1,7 @@
+# PythonWeatherTemplate
+
+To run the application, run command "python app.py"
+
+This was a proof of concept website which makes an API request to the openweather api
+
+It uses a form where a user can input a zipcode, then redirects the user to another page where it then displays the results
---
+++ app.py
@@ -0,0 +1,46 @@
+#pip install requests
+import requests
+
+#pip install configparser
+# ^allows us to read config.ini
+import configparser
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def weather_dashboard():
+    return render_template('home.html')
+
+@app.route('/results', methods=['POST'])
+def render_results():
+
+    zipcode = request.form['zipcode']
+    api_key = get_api_key()
+
+    data = get_weather_results(zipcode, api_key)
+
+    temp = "{0:.2f}".format(data["main"]["temp"])
+    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+
+    return render_template('results.html', location=location, temp=temp, feels_like=feels_like, weather=weather)
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+
+def get_weather_results(zipcode, api_key):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zipcode, api_key)
+    result = requests.get(api_url)
+    return result.json()
+
+# print(get_weather_results("94121", get_api_key()))
+
+if __name__ == '__main__':
+    app.run() 
+
+
